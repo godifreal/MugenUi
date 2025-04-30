eb88ce90976d139360e67d645df21fed2ddc4fc7
godifreal <godofreal6@gmail.com>
2025-05-01 02:01:39 +0700

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -138,7 +138,7 @@ function UI.MakeWindow(params)
 	UIStroke_3.Parent = profil
 
 	ImageLabel.Parent = profil
-	ImageLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+	ImageLabel.BackgroundColor3 = Color3.fromRGB(200, 0, 255)
 	ImageLabel.BorderColor3 = Color3.fromRGB(0, 0, 0)
 	ImageLabel.BorderSizePixel = 0
 	ImageLabel.Position = UDim2.new(0.0386740342, 0, 0.0810810775, 0)
