5423ccc9a694c501ef662167184070f6e44f949e
godifreal <godofreal6@gmail.com>
2025-04-30 21:57:01 +0700

    Update 1.lua

--- 1.lua
+++ 1.lua
@@ -170,7 +170,7 @@ function UI.MakeWindow(params)
 	TabMenu.Name = "TabMenu"
 	TabMenu.Parent = Frame
 	TabMenu.Active = true
-	TabMenu.BackgroundColor3 = Color3.fromRGB(40, 40, 40)
+	TabMenu.BackgroundColor3 = Color3.fromRGB(50, 80, 130)
 	TabMenu.BorderColor3 = Color3.fromRGB(0, 0, 0)
 	TabMenu.BorderSizePixel = 0
 	TabMenu.Size = UDim2.new(0, 181, 0, 224)
